42282f643c9e3bf4860fcf62a660a623c9dfde89
PHAMLESONHAI <phamlesonhai96@gmail.com>
2023-11-06 23:15:57 +0700

    work from home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,71 @@
-export default function Footer(){
+import { Container } from "reactstrap"
+import img1 from "../../img/logoheadphone.png"
+import "./Footer.css"
+export default function Footer() {
 
-    return(
+    return (
         <div>
-            
+            <Container className="footer">
+            <div className="row">
+                <div className="first col-md-3 col-sm-6 col-ms-12">
+                    <a href='#' className='logo1'>  <img src={img1}></img><h1 > SoundG</h1></a>
+                    <p>About SoundG</p>
+                    <a href="#" className="click">Click to read more</a>
+                </div>
+                <ul className="col-md-3 col-sm-6 col-ms-12">
+                    <p>QUESTIONS?</p>
+                    <li><a href="#">Shipping</a></li>
+                    <li><a href="#">Customer Service / FAQ's</a></li>
+                    <p>INFORMATION?</p>
+                    <li><a href="#">Our Story</a></li>
+                    <li><a href="#">Read Our Blog</a></li>
+                </ul>
+                <ul className="col-md-3 col-sm-6 col-ms-12">
+                    <p>WHY <span>SOUNDG</span></p>
+                    <li><a href="#">Shipping</a></li>
+                    <li><a href="#">30 day returns</a></li>
+                    <li><a href="#">Official stockist of all brands</a></li>
+                    <li><a href="#">Secure checkout</a></li>
+                    <li><a href="#">The best customer service</a></li>
+                </ul>
+                <ul className="col-md-3 col-sm-6 col-ms-12">
+                    <p>CONTACT <span>NECSHOP</span></p>
+                    <li><i class="fa-solid fa-location-dot"></i>8901 Marmora Road,
+                        Glasgow, D04 89GR.</li>
+                    <li><i class="fa-solid fa-phone"></i>+1 800 559 6580
+                        +1 504 889 9898</li>
+                    <li><i class="fa-regular fa-envelope"></i>mail@demolink.org</li>
+                </ul>
+                </div>
+                <div className="border"></div>
+                <div className="icon1">
+                    <div className="left">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                    <i class="fa-brands fa-pinterest"></i>
+                    <i class="fa-brands fa-google"></i>                   
+                    </div>       
+                    <label>NEWSLETTER</label>
+                    <div className="right">
+                    <input type="text"></input><button>Subcribe</button>
+                </div>
+                </div>
+                <div className="footer1">
+                <div className="left col-md-6 col-sm-6 col-xs-12 ">
+                    <ul>
+                        <li><a href="#">SOUNDG<span> © 2016.</span></a></li>
+                        <li><a href="#"> PRIVACY POLICY</a></li>
+                        <li><a href="#"> ABOUT US</a></li>
+                        <li><a href="#"> BLOG</a></li>
+                        <li><a href="#"> CONTACTS</a></li>
+                    </ul>
+                </div>
+                <div className="right col-md-6 col-sm-6 col-xs-12 ">
+
+                </div>
+                </div>
+            </Container>
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,86 @@
-import img1 from"../../img/logo-removebg-preview.png"
-export default function Header(){
+import React, { useRef, useState } from 'react';
+import "./Header.css"
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Container,
+    CardBody,
+    Card,
+    Button,
+} from 'reactstrap';
+import img1 from "../../img/logoheadphone.png"
+export default function Header() {
+    const [on, setOn] = useState(false);
+    const [on1, setOn1] = useState(false);
+    const [on2, setOn2] = useState(false);
+    const [on3, setOn3] = useState(false);
+    const sidebarRef = useRef();
+    const rightref = useRef();
+    const handle_toggle = () => {
+        sidebarRef.current.classList.toggle("active")
+        rightref.current.classList.toggle("active")
 
-    return(
-        <div className="header">
-            <h1 className="Logo"><img src={img1}></img>Farm V</h1>
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Services</li>
-                <li>Pages</li>
-                <li>Blog</li>
-                <li>Contact</li>
-            </ul>
+    }
+    const toggle = () => setOn(!on);
+    const toggle1 = () => setOn1(!on1);
+    const toggle2 = () => setOn2(!on2);
+    const toggle3 = () => setOn3(!on3);
+    return (
+        <div className='header' >
+            <a href='#' className='logo'>  <img src={img1}></img><h1 > SoundG</h1></a>
+            <button className='toggle' onClick={handle_toggle}>
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <div className='nav-side' ref={sidebarRef}>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Audio</a></li>
+                    <li><a href='#' onClick={toggle1} >
+                        Headphones
+                    </a></li>
+                    <Collapse isOpen={on1}>
+                                    home
+                                    gdfgsdaer
+                    </Collapse>
+                    <li><a href='#' onClick={toggle2} >
+                        Accessories
+                    </a></li>
+                    <Collapse isOpen={on2}>
+                                    home
+                    </Collapse>
+                    <li><a href='#' onClick={toggle3} >
+                        Shop
+                    </a></li>
+                    <Collapse isOpen={on3}>
+                                    home
+                    </Collapse>
+                     <li><a href='#'>Pages</a></li>
+                </ul>
+                <button className='close' onClick={handle_toggle}>X</button>
+            </div>
+            <div className='right' onClick={handle_toggle} ref={rightref}></div>
+            <Navbar expand="md" className="menu" color="faded" light  >
+                <Collapse className='justify-content-end text' isOpen={on} navbar>
+                    <Nav navbar >
+                        <NavLink href='#'>Home</NavLink>
+                        <NavLink href="/components/">Audio</NavLink>
+                        <NavLink href="#">Headphones</NavLink>
+                        <NavLink href='#'>Accessories</NavLink>
+                        <NavLink href='#'>Shop</NavLink>
+                        <NavLink href='#'>Pages</NavLink>
+                        <input type='type' placeholder="Search"className='search'></input><a href='#' className='icon'><i class="fa-solid fa-magnifying-glass"></i><i class="fa-solid fa-cart-shopping"></i></a>
+                    </Nav>
+                </Collapse>
+            </Navbar>
         </div>
     )
 }
